60915a28f96586505f2025be5b796d367a5e3ab8
marcoss-rrodriguez <129696308+marcoss-rrodriguez@users.noreply.github.com>
2023-12-06 14:30:29 -0600

    se actualizo app

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import config from "./config";
 import usuariosRoutes from "./routes/usuarios.routes";
 import serviciosRoutes from "./routes/servicios.routes";
+import categoriasRoutes from "./routes/categorias_servicios.routes";
+import resenasRoutes from "./routes/resenas.routes";
+import tiposUsuariosRoutes from "./routes/tipos_usuario.routes"
 
 const app = express();
 
@@ -14,6 +17,8 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api", usuariosRoutes);
 app.use("/api", serviciosRoutes);
-
+app.use("/api", categoriasRoutes);
+app.use("/api", resenasRoutes);
+app.use("/api",tiposUsuariosRoutes);
 
 export default app;
