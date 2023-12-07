cbccc9bf3f3725409a8e6edba5365d0a9d443aa2
hansel-zavala <144080076+hansel-zavala@users.noreply.github.com>
2023-12-07 00:32:16 -0600

    Create transacciones.controller.js

---
+++ src/controllers/transacciones.controller.js
@@ -0,0 +1,62 @@
+import ModeloTransacciones from '../models/transacciones.models';
+
+export const mostrarTransacciones = async (req, res) => {
+  try {
+    const transacciones = await ModeloTransacciones.findAll();
+    res.json(transacciones);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const añadirTransacciones = async (req, res) => {
+  try {
+    const nuevoTransacciones = await ModeloTransacciones.create(req.body);
+    res.json(nuevoTransacciones);
+  } catch (error) {
+    res.status(500).json({ error: error.message});
+  }
+};
+
+export const recibirTransaccionesPorId = async (req, res) => {
+  try {
+    const nuevoTransacciones = await ModeloTransacciones.findByPk(req.params.id_transaccion);
+    if (nuevoTransacciones) {
+      res.json(nuevoTransacciones);
+    } else {
+      res.status(404).json({ error: 'Transaccion no encontrado' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const borrarTransaccion = async (req, res) => {
+  try {
+    const numTransaccionesEliminados = await ModeloTransacciones.destroy({
+      where: { id_transaccion: req.params.id_transaccion }
+    });
+    if (numTransaccionesEliminados) {
+      res.json({ message: 'Transaccion eliminado con éxito' });
+    } else {
+      res.status(404).json({ error: 'Transaccion no encontrado' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const actualizarTransaccion = async (req, res) => {
+  try {
+    const TransaccionActualizado = await ModeloTransacciones.update(req.body, {
+      where: { id_transaccion: req.params.id_transaccion }
+    });
+    if (TransaccionActualizado[0] !== 0) {
+      res.json({ message: 'Transaccion actualizado con éxito' });
+    } else {
+      res.status(404).json({ error: 'Transaccion no encontrado' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
