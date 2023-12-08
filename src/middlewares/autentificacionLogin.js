f3f0cd2e5b50303ed9a073befff42502b6bcf1b8
AAxxel <144186534+AAxxel@users.noreply.github.com>
2023-12-07 23:57:17 -0600

    Update autentificacionLogin.js

--- src/middlewares/autentificacionLogin.js
+++ src/middlewares/autentificacionLogin.js
@@ -1,6 +1,6 @@
 const ModeloUsuarios = require("../models/usuarios.models.js");
 
-async function verificarLogin(usuario) { //////Cuando este finalizado enviar unicamente la variable que contenga el nombre de usuario
+async function verificarLogin(usuario) { 
     try {
       const usuarioEncontrado = await ModeloUsuarios.findOne({
         where: {
